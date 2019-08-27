cc46b94c6110ae2c0256ecfbe23e452894c86d71
edxu96 <edxu96@outlook.com>
2019-08-27 12:32:18 +0200

    update

---
+++ tests/template.R
@@ -0,0 +1,11 @@
+# ./MatrixTSA/tests/template.R: Template to Solve a Problem
+# Edward J. Xu, edxu96@outlook.com
+# Aug 26, 2019
+
+
+
+main <- function(){
+
+}
+
+main()
---
+++ tests/testthat/func/predict.R
@@ -0,0 +1,11 @@
+library(testthat)
+library(MatrixTSA)
+
+context("String length")
+library(stringr)
+
+test_that("str_length is number of characters", {
+  expect_equal(str_length("a"), 1)
+  expect_equal(str_length("ab"), 2)
+  expect_equal(str_length("abc"), 3)
+})
